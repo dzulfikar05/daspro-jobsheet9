4de2f39da6808574a54cb575f9451da9267b9be7
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2023-10-30 09:10:09 +0700

    latihan 3 percobaan 1

--- ArrayRataNilai08.java
+++ ArrayRataNilai08.java
@@ -1,4 +1,21 @@
 import java.util.Scanner;
 public class ArrayRataNilai08 {
-    
+    public static void main(String[] args) {
+        Scanner sc08 = new Scanner(System.in);
+
+        int[] nilaiMhs  = new int[10];
+        double total = 0;
+        double rata2;
+
+        for(int i = 0; i<nilaiMhs.length; i++){
+            System.out.print("Masukkan nilai akhir ke-"+(i+1)+" : ");
+            nilaiMhs[i] = sc08.nextInt();
+        }
+
+        for(int i = 0; i<10; i++){
+            total +=  nilaiMhs[i];
+        }
+        rata2 = total / nilaiMhs.length;
+        System.out.println("Rata - rata nilai = " + rata2);
+    }
 }
