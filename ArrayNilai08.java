6008d7bf89a2e0ca0c542440bae478db471f4049
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2023-10-30 09:06:07 +0700

    change name variabel

--- ArrayNilai08.java
+++ ArrayNilai08.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class ArrayNilai08 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc08 = new Scanner(System.in);
 
         int[] nilaiAkhir = new int[10];
 
         for(int i = 0; i<nilaiAkhir.length; i++){
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
-            nilaiAkhir[i] = sc.nextInt();
+            nilaiAkhir[i] = sc08.nextInt();
         }
         
         for(int i = 0; i<10; i++){
