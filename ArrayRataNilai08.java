8d918e6be1e8f72aa5a06d5027a8c329c9be22a2
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2023-10-30 10:16:35 +0700

    latihan 3 percobaan 2

--- ArrayRataNilai08.java
+++ ArrayRataNilai08.java
@@ -3,19 +3,28 @@ public class ArrayRataNilai08 {
     public static void main(String[] args) {
         Scanner sc08 = new Scanner(System.in);
 
-        int[] nilaiMhs  = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int[] nilaiMhs  = new int[sc08.nextInt()];
+        double totalLolos = 0, totalTidakLolos=0, rataL, rataT;
+        int countLolos = 0, countTidakLolos=0;
 
         for(int i = 0; i<nilaiMhs.length; i++){
             System.out.print("Masukkan nilai akhir ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc08.nextInt();
+            if(nilaiMhs[i] > 70){
+                totalLolos += nilaiMhs[i];
+                countLolos += 1;
+            }else{
+                totalTidakLolos += nilaiMhs[i];
+                countTidakLolos += 1;
+            }
         }
 
-        for(int i = 0; i<10; i++){
-            total +=  nilaiMhs[i];
-        }
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata - rata nilai = " + rata2);
+        rataL = totalLolos / countLolos;
+        rataT = totalTidakLolos / countTidakLolos;
+        System.out.println("Rata - rata nilai lulus= " + rataL);
+        System.out.println("Rata - rata nilai tidak lulus= " + rataT);
+        System.out.println("Jumlah mahasiswa yang lulus berjumlah "+countLolos);
+        System.out.println("Jumlah mahasiswa yang tidak lulus berjumlah "+countTidakLolos);
     }
 }
