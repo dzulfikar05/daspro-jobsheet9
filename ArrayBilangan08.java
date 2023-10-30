99e3b960407a74f0d9c891da923357425d77bba6
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2023-10-30 08:47:43 +0700

    latihan 1 percobaan 2

--- ArrayBilangan08.java
+++ ArrayBilangan08.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class ArrayBilangan08{
     public static void main(String[] args) {
-        int [] bil = new int[4];
-
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        int [] bil = {5, 10,14,34};
 
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+
+        // System.out.println(bil[0]);
+        // System.out.println(bil[1]);
+        // System.out.println(bil[2]);
+        // System.out.println(bil[3]);
+
+        for(int i= 0; i<=4; i++){
+            System.out.println(bil[i]);
+        }
     }
 }
